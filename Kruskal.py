b17dbbb864ca42a5c398b5f0ca81ea910f8d6af1
cvvup <mashahaidukova@yandex.ru>
2024-11-22 17:29:00 +0300

    Kruskal

---
+++ Kruskal.py
@@ -0,0 +1,49 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, u):
+        if self.parent[u] != u:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+
+    def union(self, u, v):
+        root_u = self.find(u)
+        root_v = self.find(v)
+        if root_u != root_v:
+            if self.rank[root_u] > self.rank[root_v]:
+                self.parent[root_v] = root_u
+            elif self.rank[root_u] < self.rank[root_v]:
+                self.parent[root_u] = root_v
+            else:
+                self.parent[root_v] = root_u
+                self.rank[root_u] += 1
+
+def kruskal(vertices, edges):
+    uf = UnionFind(vertices)
+    mst = []
+    total_weight = 0
+
+    edges.sort(key=lambda x: x[2])
+
+    for u, v, weight in edges:
+        if uf.find(u) != uf.find(v):
+            uf.union(u, v)
+            mst.append((u, v, weight))
+            total_weight += weight
+
+    return mst, total_weight
+
+vertices = 4
+edges = [
+    (0, 1, 10),
+    (0, 2, 6),
+    (0, 3, 5),
+    (1, 3, 15),
+    (2, 3, 4)
+]
+
+mst, total_weight = kruskal(vertices, edges)
+print("Рёбра минимального остовного дерева:", mst)
+print("Суммарный вес:", total_weight)
